22ab557801733c3b6098fd402324510b804979a4
EVanAmeron <erik.vanamerongen@fraserhealth.ca>
2025-11-30 13:13:29 -0800

    Require space after ug to avoid false positives

--- FHA.patient-order-medication-caadsi.sql
+++ FHA.patient-order-medication-caadsi.sql
@@ -63,7 +63,7 @@ SELECT TOP 100
     CONCAT_WS(', ',
         CASE WHEN dose.FullDoseInstruction LIKE '%[^A-Z]U[^A-Z]%' OR dose.FullDoseInstruction LIKE '% U %' THEN 'U (Unit)' END,
         CASE WHEN dose.FullDoseInstruction LIKE '%[^A-Z]IU[^A-Z]%' OR dose.FullDoseInstruction LIKE '% IU %' THEN 'IU (International Unit)' END,
-        CASE WHEN dose.FullDoseInstruction LIKE '%ug%' THEN 'ug (Microgram)' END,
+        CASE WHEN dose.FullDoseInstruction LIKE '%ug %' THEN 'ug (Microgram)' END,
         CASE WHEN dose.FullDoseInstruction LIKE '%cc %' OR dose.FullDoseInstruction LIKE '%CC %' THEN 'cc (Cubic Centimeter)' END
     ) AS [Flagged Abbreviation]
 
@@ -118,7 +118,7 @@ WHERE rx.Sig <> '.STK-MED'
     dose.FullDoseInstruction LIKE '% IU %' OR
     
     -- ug (microgram)
-    dose.FullDoseInstruction LIKE '%ug%' OR
+    dose.FullDoseInstruction LIKE '%ug %' OR
     
     -- cc (cubic centimeter)
     dose.FullDoseInstruction LIKE '%cc %' OR
