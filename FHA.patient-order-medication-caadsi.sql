0b9374aa41295575121a6f09b165f65642f37273
EVanAmeron <erik.vanamerongen@fraserhealth.ca>
2025-12-01 10:03:58 -0800

    Remove < and > checks completely to eliminate false positives with = signs

--- FHA.patient-order-medication-caadsi.sql
+++ FHA.patient-order-medication-caadsi.sql
@@ -69,8 +69,6 @@ SELECT TOP 100
                   dose.FullDoseInstruction LIKE '%[0-9]CC %' OR dose.FullDoseInstruction LIKE '%[0-9]CC/%' OR 
                   dose.FullDoseInstruction LIKE '% cc %' OR dose.FullDoseInstruction LIKE '% cc/%' OR 
                   dose.FullDoseInstruction LIKE '% CC %' OR dose.FullDoseInstruction LIKE '% CC/%' THEN 'cc (Cubic Centimeter)' END,
-        CASE WHEN dose.FullDoseInstruction LIKE '%<%' AND dose.FullDoseInstruction NOT LIKE '%<=%' AND dose.FullDoseInstruction NOT LIKE '%(=%' THEN '< (Less Than)' END,
-        CASE WHEN dose.FullDoseInstruction LIKE '%>%' AND dose.FullDoseInstruction NOT LIKE '%>=%' AND dose.FullDoseInstruction NOT LIKE '%)=%' THEN '> (Greater Than)' END,
         CASE WHEN dose.FullDoseInstruction LIKE '%≥%' THEN '≥ (Greater Than or Equal)' END,
         CASE WHEN dose.FullDoseInstruction LIKE '%≤%' THEN '≤ (Less Than or Equal)' END,
         CASE WHEN dose.FullDoseInstruction LIKE '%@%' THEN '@ (At Symbol)' END,
@@ -148,8 +146,6 @@ WHERE rx.Sig <> '.STK-MED'
     dose.FullDoseInstruction LIKE '% CC/%' OR
     
     -- Symbols
-    (dose.FullDoseInstruction LIKE '%<%' AND dose.FullDoseInstruction NOT LIKE '%<=%' AND dose.FullDoseInstruction NOT LIKE '%(=%') OR
-    (dose.FullDoseInstruction LIKE '%>%' AND dose.FullDoseInstruction NOT LIKE '%>=%' AND dose.FullDoseInstruction NOT LIKE '%)=%') OR
     dose.FullDoseInstruction LIKE '%≥%' OR
     dose.FullDoseInstruction LIKE '%≤%' OR
     dose.FullDoseInstruction LIKE '%@%' OR
